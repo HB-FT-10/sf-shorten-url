7b8360507bd83620097207e72c96cb95e2171d49
ld-web <lucas@ld-web.net>
2025-04-24 10:27:13 +0200

    :sparkles: add shortcode generation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ShortenUrl;
 use App\Form\ShortenUrlType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
         $shortenUrl = new ShortenUrl();
         $form = $this->createForm(ShortenUrlType::class, $shortenUrl);
@@ -20,7 +21,18 @@ final class IndexController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($shortenUrl);
+            $characters = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+            $shortCode = "";
+
+            for ($i = 0; $i < 6; $i++) {
+                $shortCode .= $characters[random_int(0, strlen($characters) - 1)];
+            }
+
+            $shortenUrl->setCode($shortCode);
+            $em->persist($shortenUrl);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre lien a été créé : ' . $shortenUrl->getCode());
         }
 
         return $this->render('index/index.html.twig', [
